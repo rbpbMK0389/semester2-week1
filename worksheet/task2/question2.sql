1636f5e4f46cbbf7d8acc1cfc760cd43c33f2ac1
rbpbMK0389 <rbpb0389@leeds.ac.uk>
2026-02-01 19:35:56 +0000

    halfway through worksheet

--- worksheet/task2/question2.sql
+++ worksheet/task2/question2.sql
@@ -1,3 +1,7 @@
+-- Work by Majeedah Khan
+
 -- Find the 5 youngest students in the database.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, DateOfBirth
+
+SELECT StudentId, FirstName, LastName, DateOfBirth FROM Student ORDER BY DateOfBirth DESC LIMIT 5;
--- worksheet/task2/question3.sql
+++ worksheet/task2/question3.sql
@@ -1,3 +1,18 @@
+-- Work by Majeedah Khan
+
 -- For each department, calculate the number of enrolments in its courses.
 -- Expected Columns:
 -- DepartmentName, TotalEnrolments
+
+
+SELECT 
+    department.DepartmentName,
+    COUNT(enrolment.EnrolmentId) AS TotalEnrolments
+FROM 
+    department
+JOIN 
+    course ON department.DepartmentId = course.DepartmentId
+JOIN 
+    enrolment ON course.CourseId = enrolment.CourseId
+GROUP BY 
+    department.DepartmentName;
